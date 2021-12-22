a8c5a7ee2daa9495d74175b4423c4ae0776d8cae
João Ceia <joao.p.ceia@gmail.com>
2021-12-22 22:27:35 +0000

    updated ex00

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -6,14 +6,14 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 00:32:00 by jpceia            #+#    #+#             */
-/*   Updated: 2021/11/22 00:56:20 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/22 22:25:21 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 template <typename T>
-void    swap(T &a, T &b)
+void swap(T &a, T &b)
 {
-    T	holder;
+    T holder;
 
     holder = a;
     a = b;
@@ -21,17 +21,13 @@ void    swap(T &a, T &b)
 }
 
 template <typename T>
-T		min(const T& a, const T& b)
+T min(const T& a, const T& b)
 {
-    if (a < b)
-        return (a);
-    return (b);
+    return (a < b ? a : b);
 }
 
 template <typename T>
-T		max(const T& a, const T& b)
+T max(const T& a, const T& b)
 {
-    if (a > b)
-        return (a);
-    return (b);
+    return (a > b ? a : b);
 }
