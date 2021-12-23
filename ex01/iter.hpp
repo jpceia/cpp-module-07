501361e03a676270673a733be36fea95d1707aa2
João Ceia <joao.p.ceia@gmail.com>
2021-12-23 00:11:38 +0000

    updated ex01

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,12 +6,19 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 00:58:06 by jpceia            #+#    #+#             */
-/*   Updated: 2021/11/22 01:05:33 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/23 00:06:40 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 template <typename T>
 void iter(T *arr, int length, void (*f)(T &))
+{
+    for (int i = 0; i < length; i++)
+        f(arr[i]);
+}
+
+template <typename T>
+void iter(T *arr, int length, void (*f)(const T&))
 {
     for (int i = 0; i < length; i++)
         f(arr[i]);
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 00:58:26 by jpceia            #+#    #+#             */
-/*   Updated: 2021/11/22 01:06:20 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/12/23 00:10:53 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,26 @@
 #include <iostream>
 
 template <typename T> 
-void print_type(T& x)
+void print_type(const T& x)
 {
     std::cout << x << std::endl;
+}
+
+template <typename T>
+void double_it(T& x)
+{
+    x = x + x;
 }
 
 int main(void)
 {
     int int_arr[] = {1, 2, 3, 4, 5};
-    iter(int_arr, sizeof(int_arr) / sizeof(int_arr[0]), &print_type<int>);
+    iter(int_arr, 5, &print_type);
+    iter(int_arr, 5, &double_it<int>);
+    std::cout << std::endl;
+    iter(int_arr, 5, &print_type);
 
     std::string str_arr[] = {"chaine1", "chaine2", "chaine3"};
-    iter(str_arr, sizeof(str_arr) / sizeof(str_arr[0]), &print_type<std::string>);
+    iter(str_arr, 3, &print_type<std::string>);
     return 0;
 }
-    
