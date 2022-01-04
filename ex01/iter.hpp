a0e9f0c0f80740082b81bbdeba69491f3683a8dd
Joao Ceia <jceia@student.42lisboa.com>
2022-01-04 12:56:57 +0000

    added template for arbitrary instatiated function

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 00:58:06 by jpceia            #+#    #+#             */
-/*   Updated: 2022/01/04 12:49:15 by jceia            ###   ########.fr       */
+/*   Updated: 2022/01/04 12:54:24 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,10 @@ void iter(T *arr, std::size_t length, void (*f)(const T&))
         f(arr[i]);
 }
 
+template <typename T, typename F>
+void iter(T *arr, std::size_t length, F f)
+{
+    for (std::size_t i = 0; i < length; i++)
         f(arr[i]);
 }
 
