f62c227f8092d95994510bf43a8e6608955dd518
Joao Ceia <jceia@student.42lisboa.com>
2022-01-04 12:56:13 +0000

    updated ex01 main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpceia <joao.p.ceia@gmail.com>             +#+  +:+       +#+        */
+/*   By: jceia <jceia@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 00:58:26 by jpceia            #+#    #+#             */
-/*   Updated: 2021/12/23 00:10:53 by jpceia           ###   ########.fr       */
+/*   Updated: 2022/01/04 12:56:03 by jceia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,16 @@ void double_it(T& x)
 
 int main(void)
 {
-    int int_arr[] = {1, 2, 3, 4, 5};
-    iter(int_arr, 5, &print_type);
-    iter(int_arr, 5, &double_it<int>);
-    std::cout << std::endl;
-    iter(int_arr, 5, &print_type);
-
-    std::string str_arr[] = {"chaine1", "chaine2", "chaine3"};
-    iter(str_arr, 3, &print_type<std::string>);
+    {
+        int int_arr[] = {1, 2, 3, 4, 5};
+        iter(int_arr, 5, &print_type);
+        iter(int_arr, 5, &double_it<int>);
+        std::cout << std::endl;
+        iter(int_arr, 5, &print_type);
+    }
+    {
+        std::string str_arr[] = {"chaine1", "chaine2", "chaine3"};
+        iter(str_arr, 3, &print_type<std::string>);
+    }
     return 0;
 }
